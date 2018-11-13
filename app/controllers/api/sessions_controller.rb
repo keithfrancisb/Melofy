8819d5ec3edb11d41b3d8caef4c6b43957181779
keithfrancisb <keithfrancisb@gmail.com>
2018-11-13 11:34:16 -0500

    Coded backend auth.

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,19 @@
 class Api::SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+
+    if @user
+      login(@user)
+      render 'api/users/show'
+    else
+      render json: ['Invalid Username and/or Password'], status: 401
+    end
+  end
+
+  def destroy
+    logout
+    render json: {}
+  end
+
 end
