5ebc35b60f6fdfffd6f7a7cb0be4b563bc87578c
keithfrancisb <keithfrancisb@gmail.com>
2018-12-12 00:49:35 -0500

    Apply react contextmenu to song items in song index.

--- frontend/components/dashboard/index/song/song_item.jsx
+++ frontend/components/dashboard/index/song/song_item.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { ContextMenu, MenuItem, ContextMenuTrigger } from 'react-contextmenu';
 // const calculateTotalTime = (duration) => {
 //
 //     const minutes = Math.floor(duration / 60);
@@ -35,49 +35,59 @@ const renderTotalTime = () => {
   }
 }
 
-const SongItem = (props) => {
-  const { song, artist, album, setupAddToPlaylist, playSong } = props;
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = (event) => {
-    if (!event.target.matches(`button${window.songId}`)) {
+class SongItem extends React.Component {
 
-      const dropdowns = document.getElementsByClassName("popupBox-song");
-      for (let i = 0; i < dropdowns.length; i++) {
-        const openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show') && openDropdown.id !== `dropDown${window.songId}`) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  };
+  constructor(props) {
+    super(props);
 
-  const liClass = props.parentId ? 'songs-list' : 'song-browse-list';
-  const divClass = props.parentId ? 'div-song-list-item' : 'div-browse-song-list-item';
+    this.contextTrigger = null;
 
-  return (
-    <div key={song.id} className={divClass} onDoubleClick={playSong(song)}>
-      <li className={liClass}>
-        <div>
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleMenu(e) {
+    if(this.contextTrigger)
+      this.contextTrigger.handleContextClick(e);
+  }
+
+  render() {
+
+    const { song, artist, album, setupAddToPlaylist, playSong } = this.props;
+
+    const liClass = this.props.parentId ? 'songs-list' : 'song-browse-list';
+    const divClass = this.props.parentId ? 'div-song-list-item' : 'div-browse-song-list-item';
+
+    return (
+      <div key={song.id} className={divClass} onDoubleClick={playSong(song)}>
+        <li className={liClass}>
           <div>
-            <span>{song.name}</span>
+            <div>
+              <span>{song.name}</span>
+            </div>
+            <div className='sub-song-info'>
+              <span>{artist.name}</span>
+              <span className="second-line-separator">•</span>
+              <span>{album.name}</span>
+            </div>
           </div>
-          <div className='sub-song-info'>
-            <span>{artist.name}</span>
-            <span className="second-line-separator">•</span>
-            <span>{album.name}</span>
+          <div className='dot-div'>
+            <ContextMenuTrigger id={`${song.id}`} ref={c => this.contextTrigger = c}>
+              <button id='popup' onClick={this.toggleMenu} className={`button${song.id}`}>
+                <img className='song-misc-logo' src='https://s3.amazonaws.com/playlist-dev/icons/noun_dot_dot_dot_white.png'></img>
+              </button>
+            </ContextMenuTrigger>
           </div>
-        </div>
-        <div className='dot-div'>
-          <button id='popup' onClick={toggleDropdown(song.id)} className={`button${song.id}`}>
-            <img className='song-misc-logo' src='https://s3.amazonaws.com/playlist-dev/icons/noun_dot_dot_dot_white.png'></img>
-          </button>
-          <div id={`dropDown${song.id}`} className='popupBox-song'>
+        </li>
+        <>
+        <ContextMenu id={`${song.id}`}>
+          <MenuItem>
             <span onClick={setupAddToPlaylist(song.id)}>Add to Playlist</span>
-          </div>
-        </div>
-      </li>
-    </div>
-  );
-};
+          </MenuItem>
+        </ContextMenu>
+        </>
+      </div>
+    );
+  }
+}
 
 export default SongItem;
