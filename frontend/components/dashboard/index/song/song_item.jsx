edd61947879efc0ff34bd253d2400acad4af22b6
keithfrancisb <keithfrancisb@gmail.com>
2018-12-23 16:53:28 -0500

    Make song items be playable with play icon.

--- frontend/components/dashboard/index/song/song_item.jsx
+++ frontend/components/dashboard/index/song/song_item.jsx
@@ -65,6 +65,7 @@ class SongItem extends React.Component {
             <div className='left-side-song-item'>
               <div className={playedColor}>
                 <span className='mini-play'></span>
+                <svg onClick={this.handlePlaySong(song)} class="icon-play" viewBox="0 0 85 100"><path fill="currentColor" d="M81 44.6c5 3 5 7.8 0 10.8L9 98.7c-5 3-9 .7-9-5V6.3c0-5.7 4-8 9-5l72 43.3z"><title>PLAY</title></path></svg>
               </div>
               <div>
                 <div>
